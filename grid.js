4a211ae6eccc89cc46b1e4fb690fbd99e7242390
doggodoge <madmangaz@gmail.com>
2023-04-16 23:52:45 +0100

    1 file modified, 1 added

--- grid.js
+++ grid.js
@@ -1,20 +1,39 @@
-function closestDivisible(squareSize) {
-  while (width % squareSize !== 0 || height % squareSize !== 0) {
-    squareSize--;
-  }
-  return squareSize;
-}
-
 class Grid {
-  constructor(squareSize) {
-    this.squareSize = closestDivisible(squareSize);
+  constructor(squareSize, width, height) {
+    this.squareSize = this.#closestDivisible(squareSize);
+    this._width = width;
+    this._height = height;
+    this._grid = this.#initializeGrid();
   }
 
   draw() {
-    for (let h = 0; h < height / this.squareSize; h++) {
-      for (let w = 0; w < width / this.squareSize; w++) {
-        square(this.squareSize * w, this.squareSize * h, this.squareSize);
+    for (const row of this._grid) {
+      for (const cell of row) {
+        console.log(cell);
+        fill(cell.fill);
+        square(cell.x, cell.y, cell.squareSize);
       }
     }
+  }
+
+  #initializeGrid() {
+    for (let h = 0; h < height / this.squareSize; h++) {
+      this._grid[h] = [];
+      for (let w = 0; w < width / this.squareSize; w++) {
+        this._grid[h][w] = {
+          squareSize: this.squareSize,
+          x: this.squareSize * w,
+          y: this.squareSize * h,
+          fill: 'white',
+        };
+      }
+    }
+  }
+
+  #closestDivisible(squareSize) {
+    while (width % squareSize !== 0 || height % squareSize !== 0) {
+      squareSize--;
+    }
+    return squareSize;
   }
 }
